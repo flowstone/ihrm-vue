641bc690722180d0e5658c25171d695b710bd400
simonxue <xueyao.me@gmail.com>
2021-01-25 17:30:38 +0800

    添加部门&部门列表

--- src/module-departments/pages/index.vue
+++ src/module-departments/pages/index.vue
@@ -29,7 +29,7 @@
               <span>
                 <i class="fa fa-university" aria-hidden="true"></i>
                 <span
-                  ><strong>{{ departData.name }}</strong></span
+                  ><strong>{{ departData.companyName }}</strong></span
                 >
               </span>
               <div class="fr">
@@ -69,7 +69,7 @@
             <!--
                     构造树形列表
                     -->
-            <el-tree :data="departData.children" :indent="20">
+            <el-tree :data="departData.depts" :indent="20">
               <div
                 class="generalClass"
                 slot-scope="{ node, data }"
@@ -113,7 +113,7 @@
                       </span>
                       <el-dropdown-menu slot="dropdown">
                         <el-dropdown-item>
-                          <el-button type="text" @click="handlAdd(data.id)"
+                          <el-button type="text" @click="handleAdd(data.id)"
                             >添加子 部门</el-button
                           >
                         </el-dropdown-item>
@@ -142,8 +142,42 @@
             </el-tree>
           </div>
         </div>
+       <!-- 添加的弹框 -->
+        <el-dialog title="编辑部门" :visible.sync="dialogFormVisible">
+          <el-form ref="dataForm" :model="formData" label-width="120px">
+            <el-form-item label="部门名称">
+              <el-input
+                v-model="formData.name"
+                placeholder="请输入部门名称"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="部门编码">
+              <el-input
+                v-model="formData.code"
+                placeholder="请输入部门编码"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="部门负责人">
+              <el-input
+                v-model="formData.manager"
+                placeholder="请输入负责人"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="部门介绍">
+              <el-input
+                v-model="formData.introduce"
+                placeholder="请输入介绍"
+              ></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button type="primary" @click="createData">确定</el-button>
+            <el-button @click="dialogFormVisible = false">取消</el-button>
+          </div>
+        </el-dialog>
       </el-card>
     </div>
+     
   </div>
 </template>
 <style rel="stylesheet/scss" lang="scss">
@@ -294,27 +328,43 @@
 </style>
 
 <script>
-import {organList} from '@/api/base/department'
+import { organList, saveOrUpdate } from "@/api/base/department";
 export default {
-    name: 'rganizations-index',
-    data() {
-        return {
-            activeName: 'first',
-            dialogFormVisible: false,
-            parentId: '',
-            departData: {},
-            formData: {}
-        }
+  name: "rganizations-index",
+  data() {
+    return {
+      activeName: "first",
+      dialogFormVisible: false,
+      parentId: "",
+      departData: {},
+      formData: {},
+    };
+  },
+  methods: {
+    getObject(params) {
+      organList().then((res) => {
+        console.log(res.data.data);
+        this.departData = res.data.data;
+      });
     },
-    methods: {
-        getObject(params) {
-            organList().then(res=> {
-                this.departData = res.data.data;
-            });
-        }
+    handlAdd() {
+        this.dialogFormVisible = true;
+        console.log("点击了按钮:", this.dialogFormVisible);
     },
-    created: function() {
-        this.getObject();
-    }
-}
+    createData() {
+      this.formData.parentId = this.parentId;
+      saveOrUpdate(this.formData).then((res) => {
+        this.$message({
+          message: res.data.message,
+          type: res.data.success ? "success" : "error",
+        });
+        location.reload();
+        this.dialogFormVisible = false;
+      });
+    },
+  },
+  created: function () {
+    this.getObject();
+  },
+};
 </script>
